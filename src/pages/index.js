124c881264264ee8577ad91ea365876556bb9d3d
igorro79 <griniov.igor@gmail.com>
2022-10-16 17:46:51 +0300

    home page markup ready

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,67 +2,41 @@ import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../components/Layout'
 import * as s from '../styles/home.module.css'
-// import ComponentTemplate from '../templates/components-page'
-const BlogIndexPage = () =>
-  // {
-  // data: {
-  //   postsAllMarkdownRemark: { edges: postEdges },
-  // },
-  // }
-  {
-    // console.log(postEdges)
-    return (
-      <Layout>
-        {/* <section className="max-w-3xl mx-auto px-2 sm:px-4 xl:max-w-5xl xl:px-0">
-        <h1 className={s.title}>Специфікація до web-ресурсу</h1>
-        <p>
-          <strong>Web-ресурс</strong> призначений для ознайомлення з основами та
-          базовими правилами мови програмування Java. Розрахований на нульовий
-          рівень знань користувача у програмуванні.
-        </p>
-        <p>
-          <strong>Web-ресурс</strong> складається з десяти сторінок із
-          питаннями. На кожній сторінці є теоретична частина, в якій дано
-          достатньо інформації для вирішення відповідного завдання.
-        </p>
-        <p>
-          <strong>Web-ресурс</strong> є адаптивним: працює на різних пристроях,
-          операційних системах і браузерах.
-        </p>
-      </section> */}
-        {/* <ComponentTemplate /> */}
-      </Layout>
-    )
+import PropTypes from 'prop-types'
+
+import { HTMLContent } from '../components/Content'
+
+export default function HomePage({ data }) {
+  console.log(data)
+  return (
+    <Layout>
+      <div className="w-full">
+        <div className="space-y-4 text-left">
+          <h1 className="text-3xl leading-12 text-gray-800 lg:text-4xl lg:leading-14 mb-2">
+            {data.markdownRemark.frontmatter.title}
+          </h1>
+        </div>
+        <HTMLContent
+          className="prose max-w-none"
+          content={data.markdownRemark.html}
+        />
+      </div>
+    </Layout>
+  )
+}
+HomePage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.object,
+  }),
+}
+export const pageQuery = graphql`
+  query {
+    markdownRemark(frontmatter: { templateKey: { eq: "home" } }, html: {}) {
+      id
+      html
+      frontmatter {
+        title
+      }
+    }
   }
-
-export default BlogIndexPage
-
-// export const pageQuery = graphql`
-//   query IndexPage {
-//     postsAllMarkdownRemark: allMarkdownRemark(
-//       sort: { fields: [frontmatter___date], order: DESC }
-//       skip: 0
-//       limit: 10
-//       filter: { frontmatter: { templateKey: { eq: "blog-post" } } }
-//     ) {
-//       edges {
-//         node {
-//           frontmatter {
-//             title
-//             description
-//           }
-//           fields {
-//             slug
-//           }
-//         }
-//       }
-//     }
-//     site {
-//       siteMetadata {
-//         siteUrl
-//         title
-//         description
-//       }
-//     }
-//   }
-// `
+`
